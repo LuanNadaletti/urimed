d5113a78f32b4d9b708688270f0dc68001cb7379
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-04 16:52:55 -0300

    initial backend commit

---
+++ backend/urimed/src/main/java/com/uri/urimed/UrimedApplication.java
@@ -0,0 +1,13 @@
+package com.uri.urimed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrimedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrimedApplication.class, args);
+	}
+
+}
