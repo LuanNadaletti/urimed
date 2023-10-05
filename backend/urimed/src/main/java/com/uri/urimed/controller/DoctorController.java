f8889992d5429d201e02f331e9442e54343a41de
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-04 22:08:32 -0300

    Created doctor controller

---
+++ backend/urimed/src/main/java/com/uri/urimed/controller/DoctorController.java
@@ -0,0 +1,17 @@
+package com.uri.urimed.controller;
+
+import com.uri.urimed.record.DoctorRegistrationData;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("doctors")
+public class DoctorController {
+
+    @PostMapping
+    public void register(@RequestBody DoctorRegistrationData data) {
+    }
+
+}
