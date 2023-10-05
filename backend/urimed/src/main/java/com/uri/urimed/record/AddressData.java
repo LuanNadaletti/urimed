2694d90e0e470bcfa3ddb5bfd0a97e37f190f45d
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-04 22:06:36 -0300

    Address and doctor record objects created

---
+++ backend/urimed/src/main/java/com/uri/urimed/record/AddressData.java
@@ -0,0 +1,4 @@
+package com.uri.urimed.record;
+
+public record AddressData(String address, String neighborhood, String cep, String city, String uf, String complement, String number) {
+}
---
+++ backend/urimed/src/main/java/com/uri/urimed/record/DoctorRegistrationData.java
@@ -0,0 +1,4 @@
+package com.uri.urimed.record;
+
+public record DoctorRegistrationData(String name, String email, String crm, AddressData address) {
+}
