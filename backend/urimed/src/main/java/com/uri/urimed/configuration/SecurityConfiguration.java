04d3502b92ff43a8e3b6d62b9ee46753bbde1ae6
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-06 14:41:01 -0300

    Criados arquivos de configuração do sistema

---
+++ backend/urimed/src/main/java/com/uri/urimed/configuration/SecurityConfiguration.java
@@ -0,0 +1,20 @@
+package com.uri.urimed.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
+
+}
