227c61566d5ed985ceb9a782c2ed9c4c5daeb594
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-30 22:06:37 -0300

    Added AddressController unit test

---
+++ backend/urimed/src/test/java/com/uri/urimed/controller/AddressControllerTests.java
@@ -0,0 +1,68 @@
+package com.uri.urimed.controller;
+
+import com.uri.urimed.model.Address;
+import com.uri.urimed.record.AddressRegistrationData;
+import com.uri.urimed.repository.AddressRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+public class AddressControllerTests {
+
+    @InjectMocks
+    private AddressController addressController;
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testSave() {
+        AddressRegistrationData registrationData = createSampleRegistrationData();
+        Address address = new Address(registrationData);
+
+        Mockito.when(addressRepository.save(address)).thenReturn(address);
+
+        ResponseEntity<String> response = addressController.save(registrationData);
+
+        Assertions.assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        Assertions.assertEquals("Address saved", response.getBody());
+    }
+
+    @Test
+    void testDelete() {
+        AddressRegistrationData registrationData = createSampleRegistrationData();
+        Address address = new Address(registrationData);
+
+        Mockito.doNothing().when(addressRepository).delete(address);
+
+        ResponseEntity<String> response = addressController.delete(registrationData);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals("Address deleted", response.getBody());
+    }
+
+    private AddressRegistrationData createSampleRegistrationData() {
+        return new AddressRegistrationData(
+                "Test Street",
+                "9999999",
+                "Test Neighborhood",
+                "Test City",
+                "Test State",
+                "99999999"
+        );
+    }
+
+}
