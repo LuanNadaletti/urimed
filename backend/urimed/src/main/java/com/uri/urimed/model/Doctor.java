4938accc3edde957f3287e9ac8459e56e6fa51b0
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-31 13:25:16 -0300

    Code refactoring

--- backend/urimed/src/main/java/com/uri/urimed/model/Doctor.java
+++ backend/urimed/src/main/java/com/uri/urimed/model/Doctor.java
@@ -1,7 +1,11 @@
 package com.uri.urimed.model;
 
 import com.uri.urimed.record.DoctorRegistrationData;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
--- backend/urimed/src/main/java/com/uri/urimed/model/Person.java
+++ backend/urimed/src/main/java/com/uri/urimed/model/Person.java
@@ -1,6 +1,16 @@
 package com.uri.urimed.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
