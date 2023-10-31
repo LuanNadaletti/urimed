5b0b27306ccb86337365628e98924cb10db035ef
LuanNadaletti <luannadaletti6@gmail.com>
2023-10-30 21:29:25 -0300

    Changed saving and deleting functions

--- backend/urimed/src/main/java/com/uri/urimed/controller/DoctorController.java
+++ backend/urimed/src/main/java/com/uri/urimed/controller/DoctorController.java
@@ -3,6 +3,8 @@ package com.uri.urimed.controller;
 import com.uri.urimed.model.Doctor;
 import com.uri.urimed.record.DoctorRegistrationData;
 import com.uri.urimed.repository.DoctorRepository;
+import com.uri.urimed.repository.PersonRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +20,22 @@ public class DoctorController {
         this.doctorRepository = doctorRepository;
     }
 
-    @PostMapping
-    public void register(@RequestBody DoctorRegistrationData data) {
-        Doctor doctor = new Doctor(data);
+    @PostMapping("save")
+    public ResponseEntity<String> save(@RequestBody DoctorRegistrationData data) {
+        try {
+            Doctor doctor = new Doctor(data);
+            doctorRepository.save(doctor);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Inconsistent data", null, ResponseEntity.badRequest().build().getStatusCode());
+        }
 
-        doctorRepository.save(doctor);
+        return new ResponseEntity<>("Doctor saved", null, ResponseEntity.ok().build().getStatusCode());
+    }
+
+    @PostMapping("delete")
+    public void delete(@RequestBody DoctorRegistrationData data) {
+        Doctor doctor = new Doctor(data);
+        doctorRepository.delete(doctor);
     }
 
 }
